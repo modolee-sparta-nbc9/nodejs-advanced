fe2a98b23eb931608b8769e72e4493450a195886
modolee <modolee.si@gmail.com>
2023-11-12 14:49:46 +0900

    chore: 프로젝트 세팅

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { SERVER_PORT } from './constants/app.constant.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Example app listening on port ${SERVER_PORT}`);
+});
