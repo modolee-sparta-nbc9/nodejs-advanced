a41a42e28fd5e0bb83476db6c3145a573d9f760b
modolee <modolee.si@gmail.com>
2023-11-13 19:48:03 +0900

    feat: 인증 Middleware 구현
    - 인증 Middleware 구현
    - 내 정보 조회 API 구현

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { authRouter } from './auth.router.js';
+import { usersRouter } from './users.router.js';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', authRouter);
+apiRouter.use('/users', usersRouter);
 
 export { apiRouter };
