c9782bc8dcf777efc8bc8bbb6d6df931fa8c1ed2
modolee <modolee.si@gmail.com>
2023-11-13 21:49:40 +0900

    feat: 상품 수정 및 삭제 API 구현

--- routers/products.router.js
+++ routers/products.router.js
@@ -117,8 +117,69 @@ productsRouter.get('/:productId', async (req, res) => {
 });
 
 // 수정
-productsRouter.put('/:productId', async (req, res) => {
+productsRouter.put('/:productId', needSignin, async (req, res) => {
   try {
+    const { productId } = req.params;
+    const { title, description, status } = req.body;
+    const { id: userId, name: userName } = res.locals.user;
+
+    // 수정 정보가 하나도 없는 경우
+    if (!title && !description && !status) {
+      return res.status(400).json({
+        success: false,
+        message: '수정 정보는 최소 한 가지 이상이어야 합니다.',
+      });
+    }
+
+    const isValidStatus = status
+      ? status === 'FOR_SALE' || status === 'SOLD_OUT'
+      : true;
+
+    if (!isValidStatus) {
+      return res.status(400).json({
+        success: false,
+        message: '지원하지 않는 상태입니다. (status: FOR_SALE | SOLD_OUT)',
+      });
+    }
+
+    // 일치하는 상품이 존재하지 않는 경우
+    const product = await Products.findByPk(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: '상품 조회에 실패했습니다.',
+      });
+    }
+
+    // 작성자ID와 인증 정보의 사용자ID가 다른 경우
+    const isProductOwner = product.userId === userId;
+    if (!isProductOwner) {
+      return res.status(403).json({
+        success: false,
+        message: '상품 수정 권한이 없습니다.',
+      });
+    }
+
+    await product.update(
+      {
+        ...(title && { title }),
+        ...(description && { description }),
+        ...(status && { status }),
+      },
+      { where: { id: productId } },
+    );
+
+    const updatedProduct = {
+      ...product.toJSON(),
+      userName,
+    };
+
+    return res.status(200).json({
+      success: true,
+      message: '상품 수정에 성공했습니다.',
+      data: updatedProduct,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -129,8 +190,42 @@ productsRouter.put('/:productId', async (req, res) => {
 });
 
 // 삭제
-productsRouter.delete('/:productId', async (req, res) => {
+productsRouter.delete('/:productId', needSignin, async (req, res) => {
   try {
+    const { productId } = req.params;
+    const { id: userId, name: userName } = res.locals.user;
+
+    // 일치하는 상품이 존재하지 않는 경우
+    const product = await Products.findByPk(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: '상품 조회에 실패했습니다.',
+      });
+    }
+
+    // 작성자ID와 인증 정보의 사용자ID가 다른 경우
+    const isProductOwner = product.userId === userId;
+    if (!isProductOwner) {
+      return res.status(403).json({
+        success: false,
+        message: '상품 삭제 권한이 없습니다.',
+      });
+    }
+
+    await product.destroy({ where: { id: productId } });
+
+    const deletedProduct = {
+      ...product.toJSON(),
+      userName,
+    };
+
+    return res.status(200).json({
+      success: true,
+      message: '상품 삭제에 성공했습니다.',
+      data: deletedProduct,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
